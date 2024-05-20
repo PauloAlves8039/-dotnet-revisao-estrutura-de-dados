989318cc1cfe8945ac02cf1a25965324a332683c
PauloAlves8039 <pj38alves@gmail.com>
2024-05-20 20:51:47 -0300

    feat: criando solução para o exercício array 01

---
+++ src/01-Arrays/EstruturaDeDados.Array01/Program.cs
@@ -0,0 +1,43 @@
+﻿using EstruturaDeDados.Utilitarios.Utils;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("********** Array(vetor) - Exibir números pares e ímpares **********");
+
+        int tamanhoDoArray = 10;
+        int[] array = new int[tamanhoDoArray];
+        int numerosPares = 0;
+        int numerosImpares = 0;
+
+        Console.WriteLine();
+
+        Utilitario.LerArray(array, tamanhoDoArray);
+        ContarParesEImpares(array, ref numerosPares, ref numerosImpares);
+
+        Console.WriteLine("\n-------- Elementos do Array --------");
+        Utilitario.ImprimirArray(array);
+
+        Console.WriteLine("\n-------- Resultado --------");
+        Console.WriteLine($"Quantidade de números pares: {numerosPares}");
+        Console.WriteLine($"Quantidade de números ímpares: {numerosImpares}");
+
+        Console.ReadKey();
+    }
+
+    private static void ContarParesEImpares(int[] array, ref int pares, ref int impares) 
+    {
+        for (int i = 0; i < array.Length; i++) 
+        {
+            if (array[i] % 2 == 0)
+            {
+                pares++;
+            }
+            else 
+            {
+                impares++;
+            }
+        }
+    }
+}
