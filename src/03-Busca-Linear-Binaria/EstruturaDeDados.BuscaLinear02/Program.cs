b3711f109aa66a773fc68d61ab0bed92f487da3a
PauloAlves8039 <pj38alves@gmail.com>
2024-05-28 19:51:43 -0300

    feat: criando solução para o exercício busca linear 02

---
+++ src/03-Busca-Linear-Binaria/EstruturaDeDados.BuscaLinear02/Program.cs
@@ -0,0 +1,40 @@
+﻿using EstruturaDeDados.Utilitarios.Utils;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("********** Contador de Ocorrências **********");
+
+        Console.Write("Informe o tamanho do array: ");
+        int tamanhoDoArray = int.Parse(Console.ReadLine());
+
+        int[] array = new int[tamanhoDoArray];
+        Utilitario.LerArray(array, tamanhoDoArray);
+
+        Console.Write("Informe o elemento que deseja contar: ");
+        int elemento = int.Parse(Console.ReadLine());
+
+        int ocorrencias = ContadorDeOcorrencias(array, elemento);
+
+        Console.WriteLine("\n-------- Resultado --------");
+        Console.WriteLine($"O elemento {elemento} aparece {ocorrencias} vezes no array.");
+
+        Console.ReadKey();
+    }
+
+    private static int ContadorDeOcorrencias(int[] array, int elemento) 
+    {
+        int contador = 0;
+
+        for (int i = 0; i < array.Length; i++) 
+        {
+            if (array[i] == elemento) 
+            {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
+}
