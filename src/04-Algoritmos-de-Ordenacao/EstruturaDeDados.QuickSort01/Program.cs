9fc0d8d737e1a965b608cce85532846944a323a6
PauloAlves8039 <pj38alves@gmail.com>
2024-06-02 14:57:12 -0300

    feat: criando solução para o exercício quick sort 01

---
+++ src/04-Algoritmos-de-Ordenacao/EstruturaDeDados.QuickSort01/Program.cs
@@ -0,0 +1,61 @@
+﻿using EstruturaDeDados.Utilitarios.Utils;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("********** Algoritmo de Ordenação Quick Sort **********");
+
+        Console.Write("Informe o tamanho do array: ");
+        int tamanhoDoArray = int.Parse(Console.ReadLine());
+
+        int[] array = new int[tamanhoDoArray];
+        Utilitario.LerArray(array, tamanhoDoArray);
+
+        Console.WriteLine("\n-------- Array original --------");
+        Utilitario.ImprimirArrayCompleto(array);
+
+        OrdenarArray(array, 0, tamanhoDoArray - 1);
+
+        Console.WriteLine("\n-------- Array ordenado --------");
+        Utilitario.ImprimirArrayCompleto(array);
+
+        Console.ReadKey();
+    }
+
+    private static void OrdenarArray(int[] array, int baixo, int alto) 
+    {
+        if (baixo < alto) 
+        {
+            int indice = ParticionarUltimoElemento(array, baixo, alto);
+            OrdenarArray(array, baixo, indice - 1);
+            OrdenarArray(array, indice + 1, alto);
+        }
+    }
+
+    private static int ParticionarUltimoElemento(int[] array, int baixo, int alto) 
+    {
+        int pivo = array[alto];
+        int i = baixo - 1;
+
+        for (int j = baixo; j < alto; j++) 
+        {
+            if (array[j] <= pivo) 
+            {
+                i++;
+                SubstituirElemento(array, i, j);
+            }
+        }
+
+        SubstituirElemento(array, i + 1, alto);
+
+        return i + 1;
+    }
+
+    private static void SubstituirElemento(int[] array, int i, int j) 
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
