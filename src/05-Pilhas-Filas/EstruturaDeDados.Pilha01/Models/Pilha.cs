b6134f9ccc0880f6c9c033af2f66e67e40ab6967
PauloAlves8039 <pj38alves@gmail.com>
2024-06-05 18:15:39 -0300

    feat: aplicando solução para o exercício pilha 01

---
+++ src/05-Pilhas-Filas/EstruturaDeDados.Pilha01/Models/Pilha.cs
@@ -0,0 +1,52 @@
+﻿namespace EstruturaDeDados.Pilha01.Models
+{
+    public class Pilha<T>
+    {
+        private T[] elementos;
+        private int topo;
+
+        public Pilha(int tamanho)
+        {
+            elementos = new T[tamanho];
+            topo = -1;
+        }
+
+        public void Empilhar(T item) 
+        {
+            if (topo == elementos.Length - 1)
+            {
+                throw new InvalidOperationException("A pilha está cheia.");
+            }
+            elementos[++topo] = item;
+        }
+
+        public T Desempilhar() 
+        {
+            if (topo == -1)
+            {
+                throw new InvalidOperationException("A pilha está vazia.");
+            }
+            return elementos[topo--];
+        }
+
+        public T Topo() 
+        {
+            if (topo == -1) 
+            {
+                throw new InvalidOperationException("A pilha está vazia.");
+            }
+
+            return elementos[topo];
+        }
+
+        public bool EstaVazia() 
+        {
+            return topo == -1;
+        }
+
+        public int Tamanho() 
+        {
+            return topo + 1;
+        }
+    }
+}
---
+++ src/05-Pilhas-Filas/EstruturaDeDados.Pilha01/Program.cs
@@ -0,0 +1,36 @@
+﻿using EstruturaDeDados.Pilha01.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("********** Estrutura de Dados Pilha **********");
+
+        Pilha<int> pilha = new Pilha<int>(5);
+
+        pilha.Empilhar(10);
+        pilha.Empilhar(20);
+        pilha.Empilhar(30);
+
+        Console.WriteLine($"Elemento no topo: {pilha.Topo()}");
+
+        Console.WriteLine($"Desempilhando: {pilha.Desempilhar()}");
+        Console.WriteLine($"Desempilhando: {pilha.Desempilhar()}");
+
+        Console.WriteLine($"Elemento no topo: {pilha.Topo()}");
+
+        pilha.Empilhar(40);
+        Console.WriteLine($"Elemento no topo: {pilha.Topo()}");
+
+        while (!pilha.EstaVazia())
+        {
+            Console.WriteLine($"Desempilhando: {pilha.Desempilhar()}");
+        }
+
+        Console.WriteLine("\n-------- Resultado --------");
+
+        Console.WriteLine($"Pilha está vazia: {pilha.EstaVazia()}");
+
+        Console.ReadKey();
+    }
+}
