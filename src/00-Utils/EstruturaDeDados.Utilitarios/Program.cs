f8cb44682361f6346eb5cb0a0ca80a0e8c8feb18
PauloAlves8039 <pj38alves@gmail.com>
2024-06-04 22:13:37 -0300

    fix: excluindo arquivos

--- src/00-Utils/EstruturaDeDados.Utilitarios/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("********** Utilitários **********");
-
-        Console.ReadKey();
-    }
-}
