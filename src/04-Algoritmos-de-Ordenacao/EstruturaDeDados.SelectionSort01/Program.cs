2bc87b2cbf45628a06abb49a70b50c9a588e4820
PauloAlves8039 <pj38alves@gmail.com>
2024-06-04 21:47:37 -0300

    fix: correção de estrutura

---
+++ src/04-Algoritmos-de-Ordenacao/EstruturaDeDados.SelectionSort01/Program.cs
@@ -0,0 +1,43 @@
+﻿using EstruturaDeDados.Utilitarios.Utils;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("********** Algoritmo de Ordenação Selection Sort **********");
+
+        Console.Write("Informe o tamanho do array: ");
+        int tamanhoDoArray = int.Parse(Console.ReadLine());
+
+        int[] array = new int[tamanhoDoArray];
+        Utilitario.LerArray(array, tamanhoDoArray);
+
+        ExecutarSelectionSort(array);
+
+        Console.WriteLine("\n-------- Resultado --------");
+        Utilitario.ImprimirArrayCompleto(array);
+
+        Console.ReadKey();
+    }
+    private static void ExecutarSelectionSort(int[] array)
+    {
+        int numero = array.Length;
+
+        for (int i = 0; i < numero - 1; i++)
+        {
+            int minIndex = i;
+
+            for (int j = i + 1; j < numero; j++)
+            {
+                if (array[j] < array[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+    }
+}
