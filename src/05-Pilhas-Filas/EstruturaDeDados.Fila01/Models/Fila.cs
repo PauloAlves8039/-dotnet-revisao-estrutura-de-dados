a663c63c7774438c5b3ccac6ffbdf8b63d82405a
PauloAlves8039 <pj38alves@gmail.com>
2024-06-07 15:13:18 -0300

    feat: aplicando solução para o exercício fila 01

---
+++ src/05-Pilhas-Filas/EstruturaDeDados.Fila01/Models/Fila.cs
@@ -0,0 +1,63 @@
+﻿namespace EstruturaDeDados.Fila01.Models
+{
+    public class Fila<T>
+    {
+        private T[] elementos;
+        private int inicio;
+        private int fim;
+        private int tamanho;
+
+        public Fila(int capacidade)
+        {
+            elementos = new T[capacidade];
+            inicio = 0;
+            fim = -1;
+            tamanho = 0;
+        }
+
+        public void Enfileirar(T item)
+        {
+            if (tamanho == elementos.Length)
+            {
+                throw new InvalidOperationException("A fila está cheia.");
+            }
+
+            fim = (fim + 1) % elementos.Length;
+            elementos[fim] = item;
+            tamanho++;
+        }
+
+        public T Desenfileirar()
+        {
+            if (tamanho == 0)
+            {
+                throw new InvalidOperationException("A fila está vazia.");
+            }
+
+            T item = elementos[inicio];
+            inicio = (inicio + 1) % elementos.Length;
+            tamanho--;
+            return item;
+        }
+
+        public T Frente()
+        {
+            if (tamanho == 0)
+            {
+                throw new InvalidOperationException("A fila está vazia.");
+            }
+
+            return elementos[inicio];
+        }
+
+        public bool EstaVazia()
+        {
+            return tamanho == 0;
+        }
+
+        public int Tamanho()
+        {
+            return tamanho;
+        }
+    }
+}
---
+++ src/05-Pilhas-Filas/EstruturaDeDados.Fila01/Program.cs
@@ -0,0 +1,36 @@
+﻿using EstruturaDeDados.Fila01.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("********** Estrutura de Dados Fila **********");
+
+        Fila<int> fila = new Fila<int>(5);
+
+        fila.Enfileirar(10);
+        fila.Enfileirar(20);
+        fila.Enfileirar(30);
+
+        Console.WriteLine($"Elemento na frente: {fila.Frente()}");
+
+        Console.WriteLine($"Desenfileirando: {fila.Desenfileirar()}");
+        Console.WriteLine($"Desenfileirando: {fila.Desenfileirar()}");
+
+        Console.WriteLine($"Elemento na frente: {fila.Frente()}");
+
+        fila.Enfileirar(40);
+        Console.WriteLine($"Elemento na frente: {fila.Frente()}");
+
+        while (!fila.EstaVazia())
+        {
+            Console.WriteLine($"Desenfileirando: {fila.Desenfileirar()}");
+        }
+
+        Console.WriteLine("\n-------- Resultado --------");
+
+        Console.WriteLine($"Fila está vazia: {fila.EstaVazia()}");
+
+        Console.ReadKey();
+    }
+}
