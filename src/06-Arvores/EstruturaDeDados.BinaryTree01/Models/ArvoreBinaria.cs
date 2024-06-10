d0765dad647f8038e8a81dbd7d36fd6afc6364ba
PauloAlves8039 <pj38alves@gmail.com>
2024-06-10 20:33:08 -0300

    feat: criando solução para o exercício arvore binaria 01

---
+++ src/06-Arvores/EstruturaDeDados.BinaryTree01/Models/ArvoreBinaria.cs
@@ -0,0 +1,73 @@
+﻿namespace EstruturaDeDados.BinaryTree01.Models
+{
+    public class ArvoreBinaria
+    {
+        private int[] arvore;
+        private int tamanho;
+
+        public ArvoreBinaria(int capacidade)
+        {
+            arvore = new int[capacidade];
+            tamanho = 0;
+        }
+
+        public void Inserir(int valor) 
+        {
+            if (tamanho == arvore.Length) 
+            {
+                Console.WriteLine("A árvore está cheia");
+                return;
+            }
+
+            arvore[tamanho] = valor;
+            tamanho++;
+        }
+
+        public void ImprimirEmOrdem(int indice) 
+        {
+            if (!VerificarIndiceValido(indice)) 
+            {
+                return;
+            }
+
+            ImprimirEmOrdem(2 * indice + 1);
+
+            Console.Write($"{arvore[indice]}  ");
+
+            ImprimirEmOrdem(2 * indice + 2);
+        }
+
+        public void ImprimirPreOrdem(int indice) 
+        {
+            if (!VerificarIndiceValido(indice))
+            {
+                return;
+            }
+
+            Console.Write($"{arvore[indice]}  ");
+
+            ImprimirPreOrdem(2 * indice + 1);
+
+            ImprimirPreOrdem(2 * indice + 2);
+        }
+
+        public void ImprimirPosOrdem(int indice) 
+        {
+            if (!VerificarIndiceValido(indice))
+            {
+                return;
+            }
+
+            ImprimirPosOrdem(2 * indice + 1);
+
+            ImprimirPosOrdem(2 * indice + 2);
+
+            Console.Write($"{arvore[indice]}  ");
+        }
+
+        private bool VerificarIndiceValido(int indice)
+        {
+            return indice < tamanho;
+        }
+    }
+}
---
+++ src/06-Arvores/EstruturaDeDados.BinaryTree01/Program.cs
@@ -0,0 +1,33 @@
+﻿using EstruturaDeDados.BinaryTree01.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("********** Estrutura de Dados BinaryTree(Árvore Binária) **********");
+
+        ArvoreBinaria arvoreBinaria = new ArvoreBinaria(10);
+
+        arvoreBinaria.Inserir(1);
+        arvoreBinaria.Inserir(2);
+        arvoreBinaria.Inserir(3);
+        arvoreBinaria.Inserir(4);
+        arvoreBinaria.Inserir(5);
+        arvoreBinaria.Inserir(6);
+        arvoreBinaria.Inserir(7);
+
+        Console.WriteLine("\nPercurso Em Ordem: ");
+        arvoreBinaria.ImprimirEmOrdem(0);
+        Console.WriteLine();
+
+        Console.WriteLine("\nPercurso Pré-Ordem: ");
+        arvoreBinaria.ImprimirPreOrdem(0);
+        Console.WriteLine();
+
+        Console.WriteLine("\nPercurso Pós-Ordem: ");
+        arvoreBinaria.ImprimirPosOrdem(0);
+        Console.WriteLine();
+
+        Console.ReadKey();
+    }
+}
